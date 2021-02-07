28aa5865da18fc90fd73313554a246bd3c7e145a
Reiq.Laptop <reiqvideos@gmail.com>
2021-02-07 15:31:45 -0400

    archivos cambiados de lugar y creada una web para el bot para poder ejecutarlo en el servicio de hosting gratis

--- bot.js
+++ bot.js
@@ -1,3 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+//settings
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//routes
+app.use(require('./routes/'));
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//listening
+app.listen(PORT, ()=>{
+    console.log('Listening on PORT:', PORT);
+});
+
 const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf('1685059923:AAH9FwL3ZpnCiQuhXRkYMMic_np0kHz0dhU');
